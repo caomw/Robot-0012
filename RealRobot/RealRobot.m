32bb30d77b5d99cea15d71915af85d1ca633a2fc
vakker <javince80@gmail.com>
2016-03-12 20:47:19 +0000

    Real robot stuff

--- RealRobot/RealRobot.m
+++ RealRobot/RealRobot.m
@@ -22,31 +22,39 @@ classdef RealRobot < handle
             obj.h=COM_OpenNXT(); 
             COM_SetDefaultNXT(obj.h); 
             
+            obj.motor=NXTMotor.empty(2,0);
             %left
-            obj.motor(1)= NXTMotor('A', 'Power',0, 'TachoLimit',0,'ActionAtTachoLimit','Brake');
+            obj.motor(1)= NXTMotor('A', 'Power',0, 'TachoLimit',0,'ActionAtTachoLimit','Brake','SmoothStart',true);
             %right
-            obj.motor(2)= NXTMotor('B', 'Power',0, 'TachoLimit',0,'ActionAtTachoLimit','Brake');
+            obj.motor(2)= NXTMotor('B', 'Power',0, 'TachoLimit',0,'ActionAtTachoLimit','Brake','SmoothStart',true);
             obj.motor(1).SendToNXT(); 
             obj.motor(2).SendToNXT();
             
-            obj.mSensor= NXTMotor('C', 'Power', 50,'SpeedRegulation',false,'TachoLimit',0,'ActionAtTachoLimit','Brake',SmoothStart,true);
+            obj.mSensor= NXTMotor('C', 'Power', 50,'SpeedRegulation',false,'TachoLimit',0,'ActionAtTachoLimit','Brake','SmoothStart',true);
         end
         
         
         function move(obj,dist)
-            theta=moveTo(dist);
-            senMotorCommand(theta,1);
+            theta=obj.moveTo(dist);
+            obj.sendMotorCommand(theta,1);
         end
         function turn(obj,angle)
-            theta=turnTo(angle);
-            senMotorCommand(theta,1);
+            theta=obj.turnTo(angle);
+            obj.sendMotorCommand(theta,1);
+        end
+        
+        function a=getSensAngle(obj)
+            
+            data=obj.mSensor.ReadFromNXT();
+            a=-data.Position;
+            
         end
         
         function scan=ultraScan(obj,wait)
             %TODO move around, and take mesaurements
             
             
-            Resolution = 5;
+            resolution = 5;
             %header1 = 'Measure in cm';
             %fid=fopen('SensorMovement.txt','w');
             %fprintf(fid, [ header1 '\n']);
@@ -54,29 +62,28 @@ classdef RealRobot < handle
             angles=dist;
             direction=1;
             
-            data=obj.mSensor.ReadFromNXT();
-
-            obj.mSensor.TachoLimit=abs(-90-data.Position);
-            obj.mSensor.Power=50*sign(-90-data.Position);
-            obj.mSensor.SendToNXT();
+            obj.moveSensor(-90,1);            
+            obj.moveSensor(90,0); 
+            angle=obj.getSensAngle();
+            c=0;
+            while angle<direction*90
+                c=c+1;
+                %obj.mSensor.SendToNXT(); 
+                %obj.mSensor.WaitFor();
+                %pause(1)
+                angle=obj.getSensAngle();
+                dist(c) = GetUltrasonic(SENSOR_1);
+                angles(c)=angle;
+                %fprintf(fid, '%f \n', R(c)');
+            end
             %if wait
                 obj.mSensor.WaitFor();
                 pause(2);
             %end
+            
             obj.mSensor.Power=50;
-            obj.mSensor.TachoLimit=Resolution;
-
-            c=0;
-            while data.Position<direction*90
-                c=c+1;
-                obj.mSensor.SendToNXT(); 
-                obj.mSensor.WaitFor();
-                pause(1)
-                data=obj.mSensor.ReadFromNXT();
-                dist(c) = GetUltrasonic(SENSOR_1);
-                angles(c)=data.Position;
-                %fprintf(fid, '%f \n', R(c)');
-            end
+            %obj.mSensor.TachoLimit=resolution;
+            
             %fclose(fid);
             NXT_PlayTone(500, 100);
             scan=[angles(dist~=-1) dist(dist~=-1)];
@@ -85,7 +92,7 @@ classdef RealRobot < handle
         
         function sendMotorCommand(obj,theta,wait)
             for i=1:2
-                obj.motor(i).Power=sign(theta(i))* 50;
+                obj.motor(i).Power=sign(theta(i))* 20;
                 obj.motor(i).TachoLimit=round(abs(theta(1)));
                 obj.motor(i).SendToNXT(); 
             end
@@ -99,8 +106,10 @@ classdef RealRobot < handle
         end
         
         function moveSensor(obj,angle,wait)
+            
             data=obj.mSensor.ReadFromNXT();
-
+            angle=-angle;
+            data.Position=data.Position;
             obj.mSensor.TachoLimit=abs(angle-data.Position);
             obj.mSensor.Power=50*sign(angle-data.Position);
             obj.mSensor.SendToNXT();
@@ -112,16 +121,16 @@ classdef RealRobot < handle
         
         function [ theta ] = turnTo(obj, angle )
             Z=obj.R*[.5 .5; 1/obj.w -1/obj.w];
-            x(1)=0;
-            x(2)=deg2rad(angle);
+            x(1,1)=0;
+            x(2,1)=-deg2rad(angle);
             theta=Z\x;
             theta=rad2deg(theta);
         end
         
         function [ theta ] = moveTo(obj,dist )
             Z=obj.R*[.5 .5; 1/obj.w -1/obj.w];
-            x(1)=dist;
-            x(2)=0;
+            x(1,1)=dist;
+            x(2,1)=0;
             theta=Z\x;
             theta=rad2deg(theta);
         end
