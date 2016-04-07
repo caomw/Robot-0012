9f60ffc880c11a8dd7679e1733a0dcab246404ed
Kaiyang Zhou <kaiyangzhou92@gmail.com>
2016-04-07 16:33:24 +0100

    New feature for PFL

--- Simulation/runSim.m
+++ Simulation/runSim.m
@@ -42,7 +42,7 @@ function [ output_args ] = runSim( adminKey )
     %if the value is 1 it will run from predefined start and target positions
     %If the number is greater than 1, the first test will be from predefined
     %positions, and the rest will be randomised.
-    numberOfrepeats = 100;
+    numberOfrepeats = 1;
 
     %Predefined start and target positions
     startPositions =  [20,20;30,20;50,70 ]; %These will change
